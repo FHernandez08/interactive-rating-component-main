5437d523428594c2d996a7171f71856956c5c397
FHernandez08 <fidelhernandez_8@hotmail.com>
2024-12-19 16:14:19 -0600

    working on styling for the updated project

---
+++ index.js
@@ -0,0 +1,5 @@
+let container = document.querySelector(".container");
+let homePage = document.querySelector(".homepage");
+let endPage = document.querySelector(".thank-you-page");
+
+let submitBtn = document.querySelector("button");
